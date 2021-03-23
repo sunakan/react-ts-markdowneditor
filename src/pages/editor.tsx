df8b488dbe57915992efe6c4c781a1d14080c9d5
sunakan <sunakan03@gmail.com>
2021-03-23 15:48:59 +0900

    Use modal for save

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -4,6 +4,9 @@ import { useStateWithStorage } from '../hooks/use_state_with_storage'
 import * as ReactMarkdown from 'react-markdown'
 import { putMemo } from '../indexeddb/memos'
 import { Button } from '../components/button'
+import { SaveModal } from '../components/save_modal'
+
+const { useState } = React
 
 const Header = styled.header`
   align-content: center;
@@ -60,15 +63,14 @@ const StorageKey = 'pages/editor:text'
 
 export const Editor: React.FC = () => {
     const [text, setText] = useStateWithStorage('', StorageKey)
-    const saveMemo = (): void => {
-        putMemo('TITLE', text)
-    }
+    const [showModal, setShowModal] = useState(false)
+
     return (
         <>
             <Header>
                 あ、Markdown Editor
                 <HeaderControl>
-                    <Button onClick={saveMemo}>
+                    <Button onClick={() => setShowModal(true)}>
                         保存する
                     </Button>
                 </HeaderControl>
@@ -82,6 +84,15 @@ export const Editor: React.FC = () => {
                     <ReactMarkdown source={text} />
                 </Preview>
             </Wrapper>
+            {showModal && (
+                <SaveModal
+                    onSave={(title: string): void => {
+                        putMemo(title, text)
+                        setShowModal(false)
+                    }}
+                    onCancel={() => setShowModal(false)}
+                />
+            )}
         </>
     )
 }
